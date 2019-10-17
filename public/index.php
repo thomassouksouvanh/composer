c47b5e8f729ef9c9b2ebd00962c4896560afcceb
thomassouksouvanh <50945938+thomassouksouvanh@users.noreply.github.com>
2019-10-17 10:29:30 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,4 +3,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use HelloWorld\SayHello;
 echo SayHello::world();
 echo'<br/>';
-echo (new App\wcs\Hello)->talk();
+use App\wcs\Hello;
+$hello = new Hello();
+echo $hello->talk();
